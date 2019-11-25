149acb6292736e0a27bbeb23efe243eea88acf80
Brent Groves <brent.groves@gmail.com>
2019-11-25 15:09:53 -0500

    changed/added code

---
+++ Plex13318.js
@@ -0,0 +1,31 @@
+// A messages service that allows to create new
+// and return all existing messages
+class Plex13318 {
+  constructor() {
+    this.messages = [];
+  }
+
+  async find() {
+    // Just return all our messages
+    return this.messages;
+  }
+
+  async create(data) {
+    // The new message is the data merged with a unique identifier
+    // using the messages length since it changes whenever we add one
+    const message = {
+      id: this.messages.length,
+      text: data.text,
+    };
+
+    // Add new message to the list
+    this.messages.push(message);
+
+    return message;
+  }
+}
+
+// now we export the class, so other modules can create Cat objects
+module.exports = {
+    Service: Plex13318
+}
---
+++ app.js
@@ -0,0 +1,81 @@
+// start with node app.js
+
+const feathers = require('@feathersjs/feathers');
+const socketio = require('@feathersjs/socketio');
+const mqtt = require('mqtt');
+const config = require('../Config13318/config.json');
+const Kep13318 = require('./Kep13318');
+const Plex13318 = require('./Plex13318');
+
+// Initialize a Feathers app
+const app = feathers();
+// Configure Socket.io real-time APIs
+app.configure(socketio());
+// Register an in-memory messages service
+app.use('/Kep13318', new Kep13318.Service());
+// Register an in-memory messages service
+app.use('/Plex13318', new Plex13318.Service());
+
+// Register a nicer error handler than the default Express one
+//app.use(express.errorHandler());
+
+// Add any new real-time connection to the `everybody` channel
+app.on('connection', connection => app.channel('everybody').join(connection));
+// Publish all events to the `everybody` channel
+app.publish(data => app.channel('everybody'));
+
+// Start the server
+app
+  .listen(config.BPGServicesPort)
+  .on('listening', () =>
+    console.log(`BPGServices listening on ${config.BPGServicesPort}`),
+  );
+
+// For good measure let's create a message
+// So our API doesn't look so empty
+app.service('Kep13318').create({
+  text: 'Hello world from the server',
+});
+// For good measure let's create a message
+// So our API doesn't look so empty
+app.service('Plex13318').create({
+  text: 'Hello world from the server',
+});
+
+let mqttClient = mqtt.connect(config.MQTT);
+
+mqttClient.on('connect', function() {
+  mqttClient.subscribe('Kep13318', function(err) {
+    if (!err) {
+      console.log('BPGServices subscribed to: Kep13318');
+    }
+  });
+  mqttClient.subscribe('Plex13318', function(err) {
+    if (!err) {
+      console.log('BPGServices subscribed to: Plex13318');
+    }
+  });
+
+});
+// message is a buffer
+mqttClient.on('message', function(topic, message) {
+  const p = JSON.parse(message.toString()); // payload is a buffer
+  console.log(`Topic is: ${topic}`);
+  let msg;
+  if('Kep13318'==topic){
+    console.log("Kep13318 message")
+    msg = `${p.TransDate}, Work Center: ${p.WorkCenter},${p.NodeId},${p.Cycle_Counter_Shift_SL}`;
+    app.service('Kep13318').create({
+      text: msg,
+    });
+  }
+  if('Plex13318'==topic){
+    console.log("Plex13318 message")
+    msg = `${p.TransDate}, ${p.Part_No},${p.Serial_No},${p.ProdServer},${p.Quantity},${p.Container_Status}`;
+    app.service('Plex13318').create({
+      text: msg,
+    });
+  }
+  console.log(msg);
+});
+
